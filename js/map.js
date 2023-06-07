508ce0a44a974baa42908be88e4e340d8031a887
Cesar Urrego <caurregoz.is@gmail.com>
2023-06-07 16:01:28 +1000

    feat: Add directions button

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3VycmVnbyIsImEiOiJjbGlsN25zM28wMXV5M2Z0aGM0cXdocjE2In0.RYVRLDE9b0YxNNDoTLK_Tw';
+
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
@@ -6,4 +7,12 @@ var map = new mapboxgl.Map({
     zoom: 14
 });
 
-const marker1 = new mapboxgl.Marker().setLngLat([151.2080391,-33.8666203]).addTo(map);
+const marker = new mapboxgl.Marker().setLngLat([151.2080391,-33.8666203]).addTo(map);
+
+var directionsButton = document.getElementById('directions-btn');
+directionsButton.addEventListener('click', function() {
+    var lat = marker.getLngLat().lat;
+    var lng = marker.getLngLat().lng;
+    var googleMapsUrl = 'https://www.google.com/maps/dir/?api=1&destination=' + lat + ',' + lng;
+    window.open(googleMapsUrl, '_blank');
+});
