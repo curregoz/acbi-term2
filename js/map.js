6241cf7880a24a5f85bc957d7dcf0d0d475029ef
Cesar Urrego <caurregoz.is@gmail.com>
2023-06-07 15:03:42 +1000

    feat: Add map

--- js/map.js
+++ js/map.js
@@ -1,26 +1,9 @@
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "beta"});
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3VycmVnbyIsImEiOiJjbGlsN25zM28wMXV5M2Z0aGM0cXdocjE2In0.RYVRLDE9b0YxNNDoTLK_Tw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [151.2080391,-33.8666203],
+    zoom: 14
+});
 
-let map;
-
-async function initMap() {
-    
-    const position = { lat: -33.8666203, lng: 151.2080391 };
-    
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    
-    map = new Map(document.getElementById("map"), {
-        zoom: 14,
-        center: position,
-        mapId: "DEMO_MAP_ID",
-    });
-    
-    const marker = new AdvancedMarkerElement({
-        map: map,
-        position: position,
-        title: "Luca Loaves",
-    });
-}
-
-initMap();
+const marker1 = new mapboxgl.Marker().setLngLat([151.2080391,-33.8666203]).addTo(map);
