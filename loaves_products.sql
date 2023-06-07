0708e14a848cb9115637317c4567cb36b55ec80a
Cesar Urrego <caurregoz.is@gmail.com>
2023-06-07 21:23:47 +1000

    feat: Update sql

--- loaves_products.sql
+++ loaves_products.sql
@@ -25,9 +25,9 @@ INSERT INTO product_types (name)
 VALUES ('product'),
        ('service');
 
-INSERT INTO `product` (`id`, `code`, `name`, `type`, `description`, `image`, `price`, `additional_price`, `schedule`) VALUES
-(1, 'SW1', 'Sourdough White', '1', 'Our standard sourdough', 'assets/sourdough_white.png', 7.00, '', ''),
-(2, 'SR1', 'Sourdough Rye', '1', 'Sourdough created with 50% rye flour', 'assets/sourdough_rye.png', 8.00, '', ''),
-(3, 'SS1', 'Sourdough Spelt', '1', 'Sourdough created with 100% spelt flour', 'assets/sourdough_splet.png', 9.00, '', '' ),
-(4, 'SS1', 'Sourdough Seeded', '1', 'Sourdough including a mixture of yummy seeds','assets/sourdough_seeded.png',  9.50, '', ''),
-(5, 'SMC1', 'Sourdough bread making classes', '2', 'Learn to make your own bread', 'assets/sourdough_making_classes.png', 350, 'GST.', 'First Saturday of every month 9 am to 5 pm with lunch provided.');
+INSERT INTO `product` (`code`, `name`, `type`, `description`, `image`, `price`, `additional_price`, `schedule`) VALUES
+('SW1', 'Sourdough White', '1', 'Our standard sourdough', 'assets/sourdough_white.webp', 7.00, '', ''),
+('SR1', 'Sourdough Rye', '1', 'Sourdough created with 50% rye flour', 'assets/sourdough_rye.webp', 8.00, '', ''),
+('SS1', 'Sourdough Spelt', '1', 'Sourdough created with 100% spelt flour', 'assets/sourdough_splet.webp', 9.00, '', '' ),
+('SS1', 'Sourdough Seeded', '1', 'Sourdough including a mixture of yummy seeds','assets/sourdough_seeded.webp',  9.50, '', ''),
+('SMC1', 'Sourdough bread making classes', '2', 'Learn to make your own bread', 'assets/sourdough_making_classes.webp', 350, 'GST.', 'First Saturday of every month 9 am to 5 pm with lunch provided.');
